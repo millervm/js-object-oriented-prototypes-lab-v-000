d1510d33e3bb0492184cd2c9e71151701e67f5b7
Veronica Miller <vmcmiller@gmail.com>
2019-02-24 19:49:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,3 +34,11 @@ BoardMember.prototype.approve = function() {
 BoardMember.prototype.doCharity = function() {
   return "I like to help people.";
 };
+
+BoardMember.prototye.releasePressStatement = function() {
+  return "You will see great things from Scuber.";
+};
+
+BoardMember.prototype.sayHi = function() {
+  return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
+};
